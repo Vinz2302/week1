2e10bb500ba96f01e58c9617428d135feea4e077
Vinz <vincentkosasih2302@gmail.com>
2023-02-22 14:26:39 +0700

    add endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const db = require('./queries')
 const port = 3000
 
 app.use(bodyParser.json())
@@ -11,9 +12,15 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-  })
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
 
-  app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-  })
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
